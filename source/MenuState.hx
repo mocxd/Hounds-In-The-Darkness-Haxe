e6899ed85c268f17d32a118ba0a6fa3c97e27d9b
mocxd <mark.adkins.hastings@gmail.com>
2015-11-24 22:56:37 -0500

    flesh out menu screen, start ship

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -5,24 +5,40 @@ import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
-import flixel.util.FlxMath;
+//import flixel.util.FlxMath;
 
 /**
  * A FlxState which can be used for the game's menu.
  */
  class MenuState extends FlxState
  {
+
+ 	private var currentSelection = 0;
+ 	private	var loadText = new FlxText(200, 333, 41, ">_", 20);
+ 	private var newText = new FlxText(380, 333, 21, "new", 20);
 	/**
-	 * Function that is called up when to state is created to set it up. 
+	 * Function that is called up when to state is created to set it up.
 	 */
 	 override public function create():Void
 	 {
-	 	add(new FlxText(400, 400, 100, "Hounds in the Darkness"));
+	 	var menuTitleBg = new FlxSprite(-200,-100);
+	 	menuTitleBg.loadGraphic("assets/images/hitd.png", false);
+	 	menuTitleBg.setGraphicSize(900, 600);
+	 	var menuTitleFg = new FlxSprite(-200,0);
+	 	menuTitleFg.loadGraphic("assets/images/hitd2.png", false);
+	 	menuTitleFg.setGraphicSize(900, 100);
+
+	 	loadText.color = 0xff80ebed;
+	 	newText.color = 0xffffffff;
+	 	add(menuTitleBg);
+	 	add(menuTitleFg);
+	 	add(loadText);
+	 	add(newText);
 	 	super.create();
 	 }
-	 
+
 	/**
-	 * Function that is called when this state is destroyed - you might want to 
+	 * Function that is called when this state is destroyed - you might want to
 	 * consider setting all objects this state uses to null to help garbage collection.
 	 */
 	 override public function destroy():Void
@@ -33,8 +49,53 @@ import flixel.util.FlxMath;
 	/**
 	 * Function that is called once every frame.
 	 */
-	 override public function update():Void
+	 override public function update(e:Float):Void
 	 {
-	 	super.update();
-	 }	
+	 	menuActions();
+	 	super.update(e);
+	 }
+
+	 private function menuActions():Void {
+	 	//var up = FlxG.keys.anyJustPressed(["UP", "W"]);
+	 	//var down = FlxG.keys.anyJustPressed(["DOWN", "S"]);
+	 	var left = FlxG.keys.anyJustPressed(["LEFT", "A"]);
+	 	var right = FlxG.keys.anyJustPressed(["RIGHT", "D"]);
+	 	var ok = FlxG.keys.anyJustPressed(["ENTER", "SPACE"]);
+
+	 	if (right) {
+	 		jogSelection(1);
+	 	} else if (left) {
+	 		jogSelection(-1);
+	 	} else if (ok) {
+	 		clickMenu();
+	 	}
+
+	 	updateMenu();
+	 }
+
+	 private function updateMenu():Void {
+	 	loadText.color = 0xffffffff;
+	 	newText.color = 0xffffffff;
+	 	if (currentSelection == 0) {
+	 		loadText.color = 0xff80ebed;
+	 	} else if (currentSelection == 1) {
+	 		newText.color = 0xff80ebed;
+	 	}
+	 }
+
+	 private function clickMenu():Void {
+	 	if (currentSelection == 1) {
+	 		FlxG.switchState(new PlayState());
+	 	}
+	 }
+
+	 private function jogSelection(delta:Int):Void {
+	 	var max = 1;
+	 	currentSelection+=delta;
+	 	if (currentSelection > max) {
+	 		currentSelection = 0;
+	 	} else if (currentSelection < 0) {
+	 		currentSelection = max;
+	 	}
+	 }
 	}
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -5,23 +5,30 @@ import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
-import flixel.util.FlxMath;
+import flixel.FlxCamera;
+import flixel.math.FlxPoint;
+//import flixel.util.FlxMath;
 
 /**
  * A FlxState which can be used for the actual gameplay.
  */
 class PlayState extends FlxState
 {
+	var playerShip = new FlxSprite(333,333);
 	/**
-	 * Function that is called up when to state is created to set it up. 
+	 * Function that is called up when to state is created to set it up.
 	 */
 	override public function create():Void
 	{
+		playerShip.loadGraphic("assets/images/miniship.png");
+		add(new FlxText(333,333, "origin"));
+		add(playerShip);
+		FlxG.camera.follow(playerShip, FlxCameraFollowStyle.NO_DEAD_ZONE);
 		super.create();
 	}
-	
+
 	/**
-	 * Function that is called when this state is destroyed - you might want to 
+	 * Function that is called when this state is destroyed - you might want to
 	 * consider setting all objects this state uses to null to help garbage collection.
 	 */
 	override public function destroy():Void
@@ -32,8 +39,25 @@ class PlayState extends FlxState
 	/**
 	 * Function that is called once every frame.
 	 */
-	override public function update():Void
+	override public function update(e:Float):Void
 	{
-		super.update();
-	}	
+		playerMovement();
+		super.update(e);
+	}
+
+	private function playerMovement():Void {
+	 	var up = FlxG.keys.anyPressed(["UP", "W"]);
+	 	var down = FlxG.keys.anyPressed(["DOWN", "S"]);
+	 	var left = FlxG.keys.anyPressed(["LEFT", "A"]);
+	 	var right = FlxG.keys.anyPressed(["RIGHT", "D"]);
+	 	if (up) {
+	 		playerShip.acceleration.x += 1;
+	 	} else if (down) {
+	 		playerShip.acceleration.x = 0;
+	 	} else if (right) {
+	 		playerShip.angle += 5;
+	 	} else if (left) {
+	 		playerShip.angle -= 5;
+	 	}
+	}
 }
